91e5170f2c93337c1aed90bbb5f5fa6452cfa7f9
kumarharsh24 <154972632+kumarharsh24@users.noreply.github.com>
2025-04-03 17:37:11 +0530

    Update tokenizer.h

--- cpp/tokenizer.h
+++ cpp/tokenizer.h
@@ -21,8 +21,10 @@ class Trie {
 public:
     Trie() {
         root = new TrieNode();
-        // Insert all stop words into the Trie
-        for (const std::string& word : stopWords) {
+        numWords = 0;
+
+        // Insert all stop words and filler words into the Trie
+        for (const std::string& word : removeWords) {
             insert(word);
         }
     }
@@ -35,7 +37,10 @@ public:
             }
             node = node->children[ch];
         }
-        node->isEndOfWord = true;
+        if (!node->isEndOfWord) {
+            node->isEndOfWord = true;
+            numWords++;
+        }
     }
 
     bool search(const std::string& word) {
@@ -49,18 +54,28 @@ public:
         return node != nullptr && node->isEndOfWord;
     }
 
-    // List of stop words
-    static const std::vector<std::string> stopWords;
+    int getNumWords() const {
+        return numWords;
+    }
+
+    // List of stop words and filler words
+    static const std::vector<std::string> removeWords;
 
 private:
     TrieNode* root;
+    int numWords;  // Track total number of words in Trie
 };
 
-// Define the list of stop words
-const std::vector<std::string> Trie::stopWords = {
+// Define the list of stop words and filler words
+const std::vector<std::string> Trie::removeWords = {
     "do", "the", "so", "at", "and", "when", "did", "they", "in", "does", "seem", 
     "beyond", "upon", "we", "ask", "about", "us", "such", "a", "of", "its", "for", 
-    "will", "it","has","had"
+    "will", "it", "has", "had",
+    // Additional filler words from Python
+    "um", "uh", "like", "you know", "so", "actually", "basically", "literally",
+    "right", "okay", "well", "i mean", "just", "kind of", "sort of", "totally",
+    "seriously", "honestly", "obviously", "really", "anyway", "thing", "stuff",
+    "maybe", "perhaps", "alright", "yeah", "no", "yes", "hmm", "hey"
 };
 
 std::vector<std::string> tokenize(const std::string& text, Trie& stopWordsTrie) {
@@ -83,6 +98,21 @@ std::vector<std::string> tokenize(const std::string& text, Trie& stopWordsTrie) 
     }
 
     return tokens;
+}
+
+// Function to compare the percentage of words from a text in the Trie
+double compareData(Trie& trie, const std::string& text) {
+    std::vector<std::string> words = tokenize(text, trie);
+    int matchCount = 0;
+
+    for (const std::string& word : words) {
+        if (trie.search(word)) {
+            matchCount++;
+        }
+    }
+
+    int numWords = trie.getNumWords();
+    return (numWords == 0) ? 0.0 : static_cast<double>(matchCount) / numWords;
 }
 
 #endif // TOKENIZER_H
